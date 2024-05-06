dd6082ac805efdc2764f47841b5246d2651e0c42
Pascoal Fernandes <147885199+Lost-Fernandes@users.noreply.github.com>
2024-05-06 17:44:04 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    setTimeout(function() {
+        document.querySelector('.PopUpZap2').className = 'PopUpZap';
+    }, 5000); // 5000 milissegundos equivalem a 5 segundos
+  };
